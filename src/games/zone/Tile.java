225be683e0d48cefa21d3ad76fd5d180792a41fa
TrisTOON <tristan.le-godais@telecomnancy.eu>
2019-05-18 00:53:45 +0200

    Constructeur de `Tile`

--- src/games/zone/Tile.java
+++ src/games/zone/Tile.java
@@ -1,4 +1,31 @@
 package games.zone;
 
+import org.newdawn.slick.Color;
+
 public class Tile {
+
+	private int type; /* Type de case */
+	private Color color; /* Couleur de la peinture recouvrant */
+
+	public Tile(int type, Color color) {
+		this.setType(type);
+		this.setColor(color);
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	public int getType() {
+		return this.type;
+	}
+
+	public Color getColor() {
+		return this.color;
+	}
+
 }
