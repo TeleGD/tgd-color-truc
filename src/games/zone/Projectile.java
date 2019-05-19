64b1fae41dad0f6fd5d110a03fa275c54c33f060
Fabien BERNIER <hundersoft@hotmail.fr>
2019-05-19 02:39:17 +0200

    Mouvement du projectile

--- src/games/zone/Projectile.java
+++ src/games/zone/Projectile.java
@@ -1,17 +1,35 @@
 package games.zone;
 
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.state.StateBasedGame;
+
 public class Projectile {
 	private int reach;
 	private int radius;
 	private int shapeType;
+	private int posX;
+	private int posY;
 	
-	public Projectile(int reach, int radius, int shapeType) {
+	public Projectile(int x0, int y0, int reach, int radius, int shapeType) {
 		this.reach = reach;
 		this.radius = radius;
 		this.shapeType = shapeType;
+		this.posX = x0;
+		this.posY = y0;
 	}
 	
-	public void projection(float direction) {
-		// direction = angle en radians
+	public void update(GameContainer container, StateBasedGame game, int delta, double speed, double direction) {
+		double speedX = speed*Math.cos(direction);
+		double speedY = speed*Math.sin(direction);
+		this.posX += (int)(speedX*delta);
+		this.posY += (int)(speedY*delta);
+	}
+	
+	public void render (GameContainer container, StateBasedGame game, Graphics context) {
+		context.setColor(Color.black);
+		// TODO: formes différentes en fonction de shapeType
+		context.fillRect(posX, posY, 16, 16);
 	}
 }
